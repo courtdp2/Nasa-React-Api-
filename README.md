29b1ac6748ebe521e79c2e4632df2e96bb39d9fb
co-deion <cdphicks.0@gmail.com>
2018-01-29 00:30:10 -0600

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Nasa-React-Api-
---
+++ nasa-react-redux/src/app/components/AstronomyContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import AstronomyCard from './AstronomyCard';
+import axios from 'axios';
+
+class AstronomyContainer extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            astronomy: []
+        }
+
+    }
+
+    componentDidMount() {
+        const API_KEY = 'dZmWvxUeso3S9XtNPrJDgNQNDQeLPOGgMhq7trCq';
+        const END_POINT = 'https://api.nasa.gov/planetary/apod?api_key='
+
+        axios.get(END_POINT+API_KEY)
+
+            .then(response => {
+
+                this.setState({
+                    astronomy: response.data
+                })
+
+                console.log(this.state.astronomy)
+            })
+            .catch(error => {
+                console.log(error, 'failed to fetch data')
+            })
+    }
+    render() {
+        const { astronomy } = this.state;
+        return (
+            <AstronomyCard data={astronomy} />
+        )
+    }
+}
+
+export default AstronomyContainer;
